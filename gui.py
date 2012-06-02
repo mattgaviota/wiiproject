0053aa0746206e05990481710ea19aa1ad179597
mattgaviota <mattgaviota@gmail.com>
2012-06-02 02:00:15 -0300

    trabajando en la interfaz de conexion

--- gui.py
+++ gui.py
@@ -27,12 +27,14 @@ class Main(QtGui.QDialog):
         QtGui.QMessageBox.information(self, u'Conectar', u'Presiona 1 + 2')
         self.wm = Wiiremote()
         self.set_initial_state()
-        self.btn_conectar.setDisabled()
+        self.btn_conectar.setDisabled(False)
 
     def set_initial_state(self):
-        self.wm.set_led = 1
-        self.spinbox_led.setValue = 1
-        self.lcd_bateria.setText(self.wm.get_battery())
+        self.wm.set_led(1)
+        self.spinbox_led.setValue(1)
+        self.lcd_bateria.display(QtCore.QString(str(self.wm.get_battery()) +
+            '%'))
+        self.lbl_rpt_mode.setText(self.wm.get_mode())
 
 def main():
     app = QtGui.QApplication(sys.argv)
